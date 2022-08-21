afcd7de30c2abb18d07de08825dc6b40a1e0ca13
alyadulya <alyadulya@gmail.com>
2022-08-21 21:15:20 +0700

    set host

--- database/index.js
+++ database/index.js
@@ -1,7 +1,25 @@
-const mongoose = require('mongoose');
-const {dbHost, dbPass, dbName, dbPort, dbUser} = require('../app/config');
+// const mongoose = require('mongoose');
+// const {dbHost, dbPass, dbName, dbPort, dbUser} = require('../app/config');
 
-mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
+// mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
+// const db = mongoose.connection;
+
+// module.exports = db;
+
+const mongoose = require('mongoose');
+const {dbHost} = require('../app/config');
+console.log(dbHost)
+try {
+    // Connect to the MongoDB cluster
+    mongoose.connect(
+      dbHost,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log(" Mongoose is connected"),
+    );
+  } catch (e) {
+    console.log("could not connect");
+}
+// mongoose.connect(dbHost);
 const db = mongoose.connection;
 
 module.exports = db;
